7bacc02f10c1ddf1bdfcbe0ba8fb4a110e87d06a
darktoni94 <tonidominguezreig@gmail.com>
2021-11-01 11:13:33 +0100

    Starts implementing language setter

---
+++ IndieDevDayGameJam/Assets/Scripts/LanguageSetter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageSetter : MonoBehaviour
+{
+    public Language language = Language.English;
+
+    public enum Language
+    {
+        English,
+        Castellano
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void CheckLanguage()
+    {
+        FindObjectOfType<TextLanguage>().SetLanguage(language);
+    }
+
+    public void SetLanguageEnglish()
+    {
+        language = Language.English;
+    }
+
+    public void SetLanguageCastellano()
+    {
+        language = Language.Castellano;
+    }
+}
---
+++ IndieDevDayGameJam/Assets/Scripts/TextLanguage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextLanguage : MonoBehaviour
+{
+    public string english;
+    public string castellano;
+
+    public void SetLanguage(LanguageSetter.Language language)
+    {
+        switch (language)
+        {
+            case LanguageSetter.Language.English:
+                GetComponent<Text>().text = english;
+                break;
+            case LanguageSetter.Language.Castellano:
+                GetComponent<Text>().text = castellano;
+                break;
+        }
+    }
+}
